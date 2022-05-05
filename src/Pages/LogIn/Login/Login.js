061169afcbb429ea58478da4b668460677d2a4bf
SohanaN <sohananaz01@gmail.com>
2022-05-06 05:31:41 +0600

    Login & Registration form added

--- src/Pages/LogIn/Login/Login.js
+++ src/Pages/LogIn/Login/Login.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import SocialLogin from '../SocialLogin/SocialLogin';
 
 const Login = () => {
+    const handleSubmit = event => {
+        event.preventDefault();
+    }
     return (
-        <div>
-            <h2>login page</h2>
-        </div>
+        <Container>
+            <div className='w-50 mx-auto my-4 text-center'>
+                <h2 className='text-success text-center mt-2'>Please Login</h2>
+                <Form onSubmit={handleSubmit} className='border border-success rounded-pill p-5 my-4'>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Control type="email" placeholder="Enter email" required />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Control type="password" placeholder="Password" required />
+                    </Form.Group>
+                    <Button variant="success w-50 mx-auto d-block mb-2 border border-success rounded-pill" type="submit">
+                        Sign In
+                    </Button>
+                </Form>
+
+                <p>New to Halal Grocery? <Link to="/register" className='text-success pe-auto text-decoration-none' > Please Register</Link> </p>
+
+                <p>Forget Password? <button className='btn btn-link text-success pe-auto text-decoration-none' >Reset Password</button> </p>
+                <SocialLogin></SocialLogin>
+            </div>
+        </Container>
     );
 };
 
--- src/Pages/LogIn/Register/Register.js
+++ src/Pages/LogIn/Register/Register.js
@@ -1,10 +1,52 @@
 import React from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import SocialLogin from '../SocialLogin/SocialLogin';
 
 const Register = () => {
+    const navigate = useNavigate();
+
+    const navigateLogin = () => {
+        navigate('/login');
+    }
+    const handleRegister = event => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+    }
     return (
-        <div>
-            <h2>register page</h2>
-        </div>
+        <Container>
+            <div className='w-50 mx-auto my-4 text-center'>
+                <h2 className='text-success text-center mt-2'>Please Register</h2>
+                <Form onSubmit={handleRegister} className='border border-success rounded-pill p-5 my-4'>
+                    <Form.Group className="mb-3" controlId="formBasicName">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="text" name="name" placeholder="Enter Your Name" required />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" name="email" placeholder="Enter email" required />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" name="password" placeholder="Password" required />
+                    </Form.Group>
+                    {/* Terms & condition checkbox */}
+                    {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Accept Terms & conditions" onClick={() => setAgree(!agree)} name="terms" className={`ps-2 ${agree ? '' : 'text-danger'}`}
+                    />
+                </Form.Group> */}
+                    <Button variant="success border border-success rounded-pill w-50 mx-auto d-block mb-2" type="submit"
+                        value="Register">
+                        Register
+                    </Button>
+                </Form>
+
+                <p>Already have an account? <Link to="/login" className='text-primary pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link> </p>
+                <SocialLogin></SocialLogin>
+            </div>
+        </Container>
     );
 };
 
