db1ab9e7329f0a96101e663fd8de4eace55670a0
SohanaN <sohananaz01@gmail.com>
2022-05-09 03:53:31 +0600

    jwt implemented

--- src/Pages/MyItems/MyItems.js
+++ src/Pages/MyItems/MyItems.js
@@ -1,22 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { Container, Row } from 'react-bootstrap';
-import axios from 'axios';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+import { useNavigate } from 'react-router-dom';
+import { signOut } from 'firebase/auth';
+import axiosPrivate from '../../api/axiosPrivate';
 
 const MyItems = () => {
-    const [restock, setRestock] = useState([]);
+
     const [user] = useAuthState(auth);
+    const [restock, setRestock] = useState([]);
+    const navigate = useNavigate();
     useEffect(() => {
         const getRestock = async () => {
             const email = user.email;
             const url = `http://localhost:4000/restock?email=${email}`;
             try {
-                const { data } = await axios.get(url);
+                const { data } = await axiosPrivate.get(url);
                 setRestock(data);
             }
             catch (error) {
-
+                if (error.response.status === 401 || error.response.status === 403) {
+                    signOut(auth);
+                    navigate('/login')
+                }
             }
         }
         getRestock();
